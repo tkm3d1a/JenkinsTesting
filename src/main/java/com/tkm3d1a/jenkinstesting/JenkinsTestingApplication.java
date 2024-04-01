154f99710855c2e7801038a07f4192b46b7021ec
Tim Klimpel <tim.klimpel@gmail.com>
2024-03-31 22:38:15 -0400

    Initial commit

---
+++ src/main/java/com/tkm3d1a/jenkinstesting/JenkinsTestingApplication.java
@@ -0,0 +1,13 @@
+package com.tkm3d1a.jenkinstesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tkm3d1a/jenkinstesting/JenkinsTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkm3d1a.jenkinstesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
